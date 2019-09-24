15540974aebf12a4f533fe8ee66e2eeea64e58c3
slscaffidi123abc <54339234+slscaffidi123abc@users.noreply.github.com>
2019-09-24 10:35:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CS_Intro1
+{
+    class Program //this our main class
+
+    {   
+        static void Main(string[] args)
+
+        {    
+            Console.WriteLine ("what time is bed time?");
+
+            //string variable
+            string sleepy = "what time is bed time?";
+            string hour = "NOT FOR FOUR MORE HOURS";
+            string sad = " i wish i was dreaming";
+
+
+            //numbers variable
+            int number1 = 10;
+            float time1 = 10.00F;
+            float time8 = 11.00F;
+            double time2 = 10.3333;
+
+            //boolean variable
+            bool boolean = true;
+            Console.WriteLine(sleepy);
+            Console.WriteLine(time1);
+            Console.WriteLine(time2);
+            Console.WriteLine(boolean);
+
+            //concatanate
+            
+            string concate1 = sleepy + hour + time1 + sad;
+            Console.WriteLine(concate1);
+
+            //multiply variable
+            float multiply = time1 * time8;
+            Console.WriteLine(multiply);
+
+
+            //constant
+            const int years = 100;
+            const int ages = 500;
+            Console.WriteLine(years);
+            Console.WriteLine(ages);
+
+            //interpolate 
+            string interpolate=  (sad + hour);
+            Console.WriteLine(interpolate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
